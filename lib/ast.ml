d074af623bde19bd8d6b69cf1e29a0ff4416833b
dalz <dalz@alsd.eu>
2025-04-10 15:56:18 +0200

    ast

---
+++ lib/ast.ml
@@ -0,0 +1,46 @@
+open! Base
+
+type ide = string
+type bop = Sum | Sub | Mul | Div | Mod
+type uop = Neg
+type cmpop = Le | Lt | Eq
+
+type aexp =
+  | Num of int
+  | Var of ide
+  | Bop of bop * aexp * aexp
+  | Uop of uop * aexp
+
+type bexp =
+  | Cmp of cmpop * aexp * aexp
+  | BAnd of bexp * bexp
+  | BOr of bexp * bexp
+  | Not of bexp
+
+type pred =
+  | True
+  | False
+  | And of pred * pred
+  | Or of pred * pred
+  | Exists of ide * pred
+  | Bool of bexp
+  | Emp
+  | PointsTo of ide * aexp
+  | PointsToNothing of ide
+  | Sep of pred * pred
+
+type cmd =
+  | Skip
+  | Assert of bexp
+  | Assign of ide * aexp
+  | AssignFromRef of ide * ide
+  | AssignToRef of ide * ide
+  | Alloc of ide
+  | Free of ide
+  | Error
+
+type prog =
+  | Cmd of cmd
+  | Seq of prog * prog
+  | Choice of prog * prog
+  | Star of prog
