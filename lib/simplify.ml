daae724af4394df9c632579927eb1497b42f674c
Massimiliano Baglioni <massimilianobaglioni@MacBook-Air-di-Massimiliano.local>
2025-04-22 18:48:35 +0200

    Created functions for Norm_prop type and completed a_exp simplification

--- lib/simplify.ml
+++ lib/simplify.ml
@@ -1,7 +1,30 @@
 open Ast
+open Norm_prop
 
 let rec simplify_a (a : aexp) : aexp =
-  match a with _ -> failwith "not implemented yet"
+  match a with
+  | Num n -> Num n
+  | Var t -> Var t
+  | Bop (a_op, a1, a2) -> (
+      let s_a1 = simplify_a a1 in
+      let s_a2 = simplify_a a2 in
+      match (s_a1, s_a1) with
+      | Num n1, Num n2 -> (
+          match a_op with
+          | Sum -> Num (n1 + n2)
+          | Sub -> Num (n1 - n2)
+          | Mul -> Num (n1 * n2)
+          | Div -> (
+              match n2 with
+              | 0 -> failwith "Attempting division by zero"
+              (* This rounds if result is not integer *)
+              | _ -> Num (n1 / n2))
+          | Mod -> Num (n1 mod n2))
+      | _ -> Bop (a_op, s_a1, s_a2))
+  | Uop (_, n) -> (
+      match n with
+      | Uop (Neg, a1) -> simplify_a a1
+      | _ -> Uop (Neg, simplify_a n))
 
 let rec simplify_b (b : bexp) : bexp =
   match b with
@@ -59,7 +82,83 @@ let rec simplify_b (b : bexp) : bexp =
               | _ -> Cmp (Eq, eval1, eval2))
           | _ -> Cmp (Eq, eval1, eval2)))
 
-let rec simplify_prop expr : prop =
+let rec simplify_atom (at : atom) : atom =
+  match at with
+  | Bool b -> Bool (simplify_b b)
+  | Emp -> Emp
+  | PointsToNothing t -> PointsToNothing t
+  | PointsTo (t, a) -> PointsTo (t, simplify_a a)
+
+(* TODO check that this works as expected *)
+(* !!! copy and pasted *)
+(* Utility to remove duplicates from a list*)
+let rec remove_from_the_right l =
+  match l with
+  | [] -> []
+  | h :: t -> h :: remove_from_the_right (List.filter (fun x -> x <> h) t)
+
+let rec simplify_conj (atoms : conj) : conj =
+  (* Extract atoms list from conj *)
+  let atoms_list = match atoms with Conj a -> a in
+  (* Simplify atoms *)
+  let simpl_list = List.map (function at -> simplify_atom at) atoms_list in
+  (* Remove AND true *)
+  let filt_list =
+    (* TODO check if this filter works as expected *)
+    List.filter (function at -> at != Bool (BConst true)) simpl_list
+  in
+  (* Remove duplicates *)
+  let unique_list = remove_from_the_right filt_list in
+  (* Checks if there is False *)
+  if List.exists (function at -> at == Bool (BConst false)) unique_list then
+    Conj [ Bool (BConst false) ]
+  else Conj unique_list
+(* TODO check if x -> v and x -> w / v!=w *)
+
+let rec simplify_sepj (conjs : sepj) : sepj =
+  (* Extract conjs from sepj *)
+  let conjs_list = match conjs with Sepj s -> s in
+
+  (* Simplify each conj in the list *)
+  let simpl_list =
+    List.map (function conj -> simplify_conj conj) conjs_list
+  in
+  (* Remove * empt from the list of conjs *)
+  let filter_list =
+    List.filter (function conj_exp -> conj_exp != Conj [ Emp ]) simpl_list
+  in
+  (* Remove duplicates *)
+  let unique_list = remove_from_the_right filter_list in
+  Sepj unique_list
+(* TODO, for each varaible check x -> v * x -> w = false *)
+
+let rec simply_dsj (sepjs : disj) : disj =
+  (* Extract the list of sepjs from disj *)
+  let sep_list = match sepjs with Disj d -> d in
+  (* Simplify each sepj inside the list *)
+  let simpl_list = List.map (function sep -> simplify_sepj sep) sep_list in
+  (* Removign OR false *)
+  let filt_list =
+    List.filter
+      (function at -> at != Sepj [ Conj [ Bool (BConst false) ] ])
+      simpl_list
+  in
+  (* Removing duplicates *)
+  let unique_list = remove_from_the_right filt_list in
+  if
+    (* Check if there is OR true *)
+    List.exists
+      (function at -> at == Sepj [ Conj [ Bool (BConst true) ] ])
+      unique_list
+  then Disj [ Sepj [ Conj [ Bool (BConst true) ] ] ]
+  else Disj unique_list
+
+let simplify_t (proposition : t) : t =
+  match proposition with
+  | id_list, disjunction -> (id_list, simply_dsj disjunction)
+
+(* TODO eventually remove simplify prop *)
+let rec simplify_prop expr =
   match expr with
   | Or (prop1, prop2) -> (
       let eval1 = simplify_prop prop1 in
--- test/test_simplify.ml
+++ test/test_simplify.ml
@@ -4,53 +4,33 @@ open Lis_project.Simplify
 let a_true = Atom (Bool (BConst true))
 let a_false = Atom (Bool (BConst false))
 
-
 (* let a_bool_true = Atom (Bool (Bool true))
 let a_bool_false = Atom (Bool (Bool false)) *)
 
 let assert_eq (name : string) (got : prop) (expected : prop) =
-  if got = expected then
-    Printf.printf "PASS: %s\n" name
+  if got = expected then Printf.printf "PASS: %s\n" name
   else
-    Printf.printf "FAIL: %s\n  got: %s\n  expected: %s\n"
-      name (show_prop got) (show_prop expected)
+    Printf.printf "FAIL: %s\n  got: %s\n  expected: %s\n" name (show_prop got)
+      (show_prop expected)
 
 let () =
-  assert_eq "And(True, True)"
-    (simplify_prop (And (a_true, a_true)))
-    a_true;
+  assert_eq "And(True, True)" (simplify_prop (And (a_true, a_true))) a_true;
 
-  assert_eq "And(True, False)"
-    (simplify_prop (And (a_true, a_false)))
-    a_false;
-  
-  assert_eq "And(False, False)"
-    (simplify_prop (And (a_false, a_false)))
-    a_false;
+  assert_eq "And(True, False)" (simplify_prop (And (a_true, a_false))) a_false;
 
-  assert_eq "And(False, True)"
-    (simplify_prop (And (a_false, a_true)))
-    a_false;
+  assert_eq "And(False, False)" (simplify_prop (And (a_false, a_false))) a_false;
 
-  assert_eq "And(False, False)"
-    (simplify_prop (And (a_false, a_false)))
-    a_false;
+  assert_eq "And(False, True)" (simplify_prop (And (a_false, a_true))) a_false;
 
-  assert_eq "Or(True, True)"
-    (simplify_prop (Or (a_true, a_true)))
-    a_true;
+  assert_eq "And(False, False)" (simplify_prop (And (a_false, a_false))) a_false;
 
-  assert_eq "Or(True, False)"
-    (simplify_prop (Or (a_true, a_false)))
-    a_true;
+  assert_eq "Or(True, True)" (simplify_prop (Or (a_true, a_true))) a_true;
 
-  assert_eq "Or(False, True)"
-    (simplify_prop (Or (a_false, a_true)))
-    a_true;
+  assert_eq "Or(True, False)" (simplify_prop (Or (a_true, a_false))) a_true;
 
-  assert_eq "Or(False, False)"
-    (simplify_prop (Or (a_false, a_false)))
-    a_false;
+  assert_eq "Or(False, True)" (simplify_prop (Or (a_false, a_true))) a_true;
+
+  assert_eq "Or(False, False)" (simplify_prop (Or (a_false, a_false))) a_false;
 
   assert_eq "Nested And Or"
     (simplify_prop (And (a_true, Or (a_false, a_true))))
@@ -67,23 +47,19 @@ let () =
   assert_eq "Cmp(Le, 1, 2)"
     (simplify_prop (Atom (Bool (Cmp (Le, Num 1, Num 2)))))
     a_true;
-  
+
   assert_eq "Cmp(Le, 2, 1)"
     (simplify_prop (Atom (Bool (Cmp (Le, Num 2, Num 1)))))
     a_false;
 
-    assert_eq "Sep(True, empty)"
-    (simplify_prop (Sep (a_true, Atom Emp)))
-    a_true;
+  assert_eq "Sep(True, empty)" (simplify_prop (Sep (a_true, Atom Emp))) a_true;
 
   assert_eq "Sep(False, empty)"
     (simplify_prop (Sep (a_false, Atom Emp)))
     a_false;
 
-  assert_eq "Sep(empty, True)"
-    (simplify_prop (Sep (Atom Emp, a_true)))
-    a_true;
+  assert_eq "Sep(empty, True)" (simplify_prop (Sep (Atom Emp, a_true))) a_true;
 
   assert_eq "Sep(empty, False)"
     (simplify_prop (Sep (Atom Emp, a_false)))
-    a_false;
+    a_false
