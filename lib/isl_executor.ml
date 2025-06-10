83a9d6c2bc393f3bc82636124fe2871848562e71
Sal <salerno.salvatore1997@gmail.com>
2025-06-10 16:57:22 +0200

    Reformatted choice_rule: better commented and now the user can also decide to let the function choose at random.

--- lib/isl_executor.ml
+++ lib/isl_executor.ml
@@ -24,7 +24,8 @@ let alloc_rule s x =
       dummies = Map.set s.dummies ~key:x ~data:x';
     }
 
-(** Let the user decide which path to take in the choice rule. *)
+(** Auxiliary function of choice_rule. It Lets the user decide which path to
+    take *)
 let rec interactive_choice_rule s p1 p2 =
   (* Printing message to the user *)
   Stdio.Out_channel.print_endline "Non-det choice between:\n\t- left_path:";
@@ -32,31 +33,35 @@ let rec interactive_choice_rule s p1 p2 =
   Stdio.Out_channel.print_endline "\n\t- right_path:";
   PPrint.ToChannel.pretty 1. 60 Out_channel.stdout (Prog.pretty p2);
   Stdio.Out_channel.print_endline
-    "Input 'L' for selecting the left path or 'R' for the right one or 'B' for \
-     doing both.";
+    "Input 'LF' for selecting the left path, 'RT' for the right path, 'BT' for \
+     doing both and 'RN' for choosing at random (default is 'RN').";
 
   (* Getting user's choice from stdin *)
   let user_choice =
-    Option.value (In_channel.input_line In_channel.stdin) ~default:"L"
+    Option.value (In_channel.input_line In_channel.stdin) ~default:"R"
   in
 
   (* Going through the selected path *)
   match user_choice with
-  | "L" -> [ (s, p1) ]
-  | "R" -> [ (s, p2) ]
-  | "B" -> [ (s, p1); (s, p2) ]
+  | "LF" -> 0
+  | "RT" -> 1
+  | "BT" -> 3
+  | "RN" -> Random.int 3
   | _ ->
       Stdio.Out_channel.output_string Stdio.stdout "Invalid input, retrying...";
       interactive_choice_rule s p1 p2
 
-(** Executes the choice rule. The decision of the branch is by default given to
-    the user. If `interactive` is setted to `false` then the program chooses at
-    random *)
+(** Executes the choice rule. If `interactive` is enabled, then the user decides
+    which path to take, otherwise the function selects at random. *)
 let choice_rule ?(interactive = true) (s : Executor_state.t) p1 p2 =
-  if interactive then interactive_choice_rule s p1 p2
-  else
-    let random_choice = Random.int 2 in
-    if random_choice = 0 then [ (s, p1) ] else [ (s, p2) ]
+  let choice =
+    if interactive then interactive_choice_rule s p1 p2 else Random.int 3
+  in
+  match choice with
+  | 0 -> [ (s, p1) ]
+  | 1 -> [ (s, p2) ]
+  | 2 -> [ (s, p1); (s, p2) ]
+  | _ -> failwith "In choice rule: choice >= 3"
 
 let iter_rule exec ( let* ) s p =
   let rec unroll s n =
